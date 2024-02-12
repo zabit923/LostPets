a406cca69203152705d8db078bb6f8e354ebefa1
zabit <xaclafun1991@gmail.com>
2024-02-12 20:11:22 +0300

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
 
     'users',
 ]
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -73,6 +75,9 @@ DATABASES = {
     }
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500"
+]
 
 # _____________________________________________________________________________________________________
 
--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,3 +1,48 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const openPopUpLogin = document.getElementById('open_pop_up_login');
+    const closePopUpLogin = document.getElementById('close_pop_up_login');
+    const popUpLogin = document.getElementById('pop_up_login');
+
+    const loginForm = document.getElementById('loginForm');
+
+
+    openPopUpLogin.addEventListener('click', function(e){
+        e.preventDefault();
+        popUpLogin.classList.add('active');
+    });
+
+    closePopUpLogin.addEventListener('click', () => {
+        popUpLogin.classList.remove('active');
+    });
+
+
     
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        fetch('http://127.0.0.1:8000/api/token/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const token = data;
+            console.log('Bearer token:', token);
+        })
+        .catch(error => {
+            console.error('There was a problem with the request:', error);
+            console.log('Response from server:', error.response);
+        });        
+    });
 });
